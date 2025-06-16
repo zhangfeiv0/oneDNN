76eed13c9f267b105641d461e4f2d225230dfa83
Kealan Barbieri <kealan.barbieri@intel.com>
2025-06-16 11:23:40 -0700

    xe: jit: gemm: enable lazy repack for fp4

--- src/gpu/intel/jit/gemm/generator/pieces/gemm_setup.cxx
+++ src/gpu/intel/jit/gemm/generator/pieces/gemm_setup.cxx
@@ -1555,7 +1555,7 @@ bool BLASKernelGenerator<hw>::gemmAccumulateCSetup(GEMMProblem &problem, GEMMStr
         // Repacked data can use significantly more registers than the loaded
         // data. Lazy repacking can reduce register utilization and improve load
         // pipelining at (in some cases) the expense of more work.
-        bool lazyRepack = state.Ta_load.isInt4() && one_of(Ta, Type::f16, Type::bf16, Type::f32);    // Other cases are unimplemented
+        bool lazyRepack = state.Ta_load.is4Bit() && one_of(Ta, Type::f16, Type::bf16, Type::f32);    // Other cases are unimplemented
         if (lazyRepack)
             state.ka_repack = std::min(state.ka_repack, strategy.kb_load);
         makeUnbackedRegLayout(Ta, state.Ar_layout, unrollM, state.ka_repack, isLayoutColMajor(state.A_layout), crosspackA, tileM_A, tileK_A, true, splitA);
