5e09f60c9eaaa24561d264cdb09e592ba7292340
Simon Ewing <simon.ewing@intel.com>
2025-07-06 23:04:41 -0700

    gpu: fixup generic vendor build dependencies

--- src/gpu/gpu_sdpa_list.cpp
+++ src/gpu/gpu_sdpa_list.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2024 Intel Corporation
+* Copyright 2024-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -18,8 +18,10 @@
 
 #include "gpu/gpu_impl_list.hpp"
 
+#if DNNL_GPU_VENDOR == DNNL_VENDOR_INTEL
 #include "gpu/intel/ocl/micro_sdpa.hpp"
 #include "gpu/intel/ocl/ref_sdpa.hpp"
+#endif
 
 namespace dnnl {
 namespace impl {
