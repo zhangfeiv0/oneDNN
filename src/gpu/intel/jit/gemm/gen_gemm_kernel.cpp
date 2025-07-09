d46fbb3082c7d8c51996e7d5243ea9223ed220de
Kealan Barbieri <kealan.barbieri@intel.com>
2025-07-09 10:08:52 -0700

    xe: jit: gemm: fixup fp8 decomp

--- src/gpu/intel/jit/gemm/gen_gemm_kernel.cpp
+++ src/gpu/intel/jit/gemm/gen_gemm_kernel.cpp
@@ -594,7 +594,6 @@ status_t gen_gemm_nocopy_kernel_desc_t::select_kernel(compute::gpu_arch_t arch,
     add_mode_matches(fpmath_bf16, [](Type dt) -> const char * {
         if (dt == Type::f32) { return "[SB]"; }
         if (dt.isInt8() || dt.isInt4()) return "[OB]";
-        if (dt.isF8()) return "B";
         if (dt.isF4()) return "F";
         return nullptr;
     });
@@ -602,7 +601,6 @@ status_t gen_gemm_nocopy_kernel_desc_t::select_kernel(compute::gpu_arch_t arch,
     add_mode_matches(fpmath_f16, [](Type dt) -> const char * {
         if (dt == Type::f32) { return "[SH]"; }
         if (dt.isInt8() || dt.isInt4()) return "[OH]";
-        if (dt.isF8()) return "H";
         if (dt.isF4()) return "F";
         return nullptr;
     });
