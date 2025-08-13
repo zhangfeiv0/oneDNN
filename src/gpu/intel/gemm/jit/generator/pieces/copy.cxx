7456d454d821bd73b3a0f0927a8ae598a6ac51b1
Youssif, Daniel <daniel.youssif@intel.com>
2025-08-13 14:25:44 -0700

    xe: jit: gemm: fixup: apply sync in copy_plan only for Xe2

--- src/gpu/intel/gemm/jit/generator/pieces/copy.cxx
+++ src/gpu/intel/gemm/jit/generator/pieces/copy.cxx
@@ -247,7 +247,8 @@ void Generator<hw>::copyExecute(CopyPlan &&plan, CommonState &state)
             mov(1, savedFlags[i], FlagRegister(i));
 
     // Workaround probable scheduling / hw issue
-    sync(SyncFunction::nop, SWSB<uint32_t>(1));
+    if (hw == HW::Xe2)
+	    sync(SyncFunction::nop, SWSB<uint32_t>(1));
 
      // Generate code.
     plan.execute(*this);
