1f27b20edf9274b4d5ff4da01647033effe64f71
Kassen, Andrew <andrew.kassen@intel.com>
2025-08-27 11:07:58 -0700

    xe: jit: fix build issue

--- src/gpu/intel/jit/ir/blocking.cpp
+++ src/gpu/intel/jit/ir/blocking.cpp
@@ -14,6 +14,7 @@
 * limitations under the License.
 *******************************************************************************/
 
+#include <numeric>
 #include <random>
 
 #include "gpu/intel/jit/ir/blocking.hpp"
