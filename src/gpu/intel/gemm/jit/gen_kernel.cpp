04fa72d1fda52380081714e2ae796839814313d1
Simon Ewing <simon.ewing@intel.com>
2025-09-17 10:51:54 -0700

    xe: gemm: use external types for searching catalog entries

--- src/gpu/intel/gemm/jit/gen_kernel.cpp
+++ src/gpu/intel/gemm/jit/gen_kernel.cpp
@@ -644,18 +644,18 @@ status_t gen_nocopy_desc_t::select_kernel(compute::gpu_arch_t arch,
     bool fpmath_f16 = mode & mode_f16x1;
 
     auto add_matches = [&](MatchParams start, const char *(*match)(Type)) {
-        if (match(problem_.Ta)) {
+        if (match(problem_.Ta_ext)) {
             match_params.push_back(start);
-            match_params.back().selector.precisions[0] = match(problem_.Ta);
+            match_params.back().selector.precisions[0] = match(problem_.Ta_ext);
         }
-        if (match(problem_.Tb)) {
+        if (match(problem_.Tb_ext)) {
             match_params.push_back(start);
-            match_params.back().selector.precisions[1] = match(problem_.Tb);
+            match_params.back().selector.precisions[1] = match(problem_.Tb_ext);
         }
-        if (match(problem_.Ta) && match(problem_.Tb)) {
+        if (match(problem_.Ta_ext) && match(problem_.Tb_ext)) {
             match_params.push_back(start);
-            match_params.back().selector.precisions[0] = match(problem_.Ta);
-            match_params.back().selector.precisions[1] = match(problem_.Tb);
+            match_params.back().selector.precisions[0] = match(problem_.Ta_ext);
+            match_params.back().selector.precisions[1] = match(problem_.Tb_ext);
         }
     };
 
