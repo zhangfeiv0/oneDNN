d1e767cecc0b35cd7a9f6021ea919ff5d72542bf
Ryo Suzuki <ryo.suzuki@arm.com>
2025-12-04 13:58:46 +0000

    ci: aarch64: Improve ci & minor bug fixes (#4387)

--- .github/automation/performance/benchdnn_comparison.py
+++ .github/automation/performance/benchdnn_comparison.py
@@ -109,23 +109,35 @@ def compare_two_benchdnn(file1, file2, out_file=None):
         r2_med_exec = statistics.median(exec2)
         r1_mean_exec = statistics.mean(exec1)
         r2_mean_exec = statistics.mean(exec2)
-        r1_sem_exec = statistics.stdev(exec1) / math.sqrt(r1_mean_exec)
-        r2_sem_exec = statistics.stdev(exec2) / math.sqrt(r2_mean_exec)
         r1_med_ctime = statistics.median(ctime1)
         r2_med_ctime = statistics.median(ctime2)
         r1_mean_ctime = statistics.mean(ctime1)
         r2_mean_ctime = statistics.mean(ctime2)
-        r1_sem_ctime = statistics.stdev(ctime1) / math.sqrt(r1_mean_ctime)
-        r2_sem_ctime = statistics.stdev(ctime2) / math.sqrt(r2_mean_ctime)
+
         use_ttest = len(exec1) >= 3 and len(exec2) >= 3
 
-        if 0 in [r1_med_exec, min(exec1), r1_med_ctime, min(ctime1)]:
+        if 0 in [
+            r1_med_exec,
+            min(exec1),
+            min(exec2),
+            r1_med_ctime,
+            min(ctime1),
+            r1_mean_exec,
+            r2_mean_exec,
+            r1_mean_ctime,
+            r2_mean_ctime,
+        ]:
             warnings.warn(
                 f"Avoiding division by 0 for {prb}. "
                 f"Exec median: {r1_med_exec}, min: {min(exec1)}; "
                 f"Ctime median: {r1_med_ctime}, min: {min(ctime1)}"
             )
             continue
+
+        r1_sem_exec = statistics.stdev(exec1) / math.sqrt(r1_mean_exec)
+        r2_sem_exec = statistics.stdev(exec2) / math.sqrt(r2_mean_exec)
+        r1_sem_ctime = statistics.stdev(ctime1) / math.sqrt(r1_mean_ctime)
+        r2_sem_ctime = statistics.stdev(ctime2) / math.sqrt(r2_mean_ctime)
 
         # A test fails if execution time:
         # - shows a statistically significant regression and
@@ -149,18 +161,18 @@ def compare_two_benchdnn(file1, file2, out_file=None):
         if exec_regressed:
             exec_failures.append(
                 f"{prb} exec: "
-                f"{r1_mean_exec:.3g}\u00B1{r1_sem_exec:.3g} "
+                f"{r1_mean_exec:.3g}\u00b1{r1_sem_exec:.3g} "
                 "\u2192 "
-                f"{r2_mean_exec:.3g}\u00B1{r2_sem_exec:.3g} "
+                f"{r2_mean_exec:.3g}\u00b1{r2_sem_exec:.3g} "
                 f"(p={exec_regressed_ttest.pvalue:.3g})"
             )
 
         if ctime_regressed:
             ctime_failures.append(
                 f"{prb} ctime: "
-                f"{r1_mean_ctime:.3g}\u00B1{r1_sem_ctime} "
+                f"{r1_mean_ctime:.3g}\u00b1{r1_sem_ctime} "
                 "\u2192 "
-                f"{r2_mean_ctime:.3g}\u00B1{r2_sem_ctime} "
+                f"{r2_mean_ctime:.3g}\u00b1{r2_sem_ctime} "
                 f"(p={ctime_ttest.pvalue:.3g})"
             )
 
