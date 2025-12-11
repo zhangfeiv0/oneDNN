dcc78e92bc176511346830586dc62e5d7feafc8c
Sergey Kazakov <sergey.kazakov@intel.com>
2025-12-11 07:36:52 -0800

    xe: gemm: allow host scalar limited support in ZP check

--- src/gpu/intel/gemm/jit/pd.cpp
+++ src/gpu/intel/gemm/jit/pd.cpp
@@ -290,6 +290,7 @@ bool pd_t::zp_ok() {
     auto &attr_zps = attr()->zero_points_;
     auto &a_zps = attr_zps.get(DNNL_ARG_A);
     auto &b_zps = attr_zps.get(DNNL_ARG_B);
+    auto &c_zps = attr_zps.get(DNNL_ARG_C);
 
     // INT4 ZPs on SRC do not expand the range in a meaningful way, skipping
     if (utils::one_of((swap_ab() ? a_zps : b_zps).get_data_type(), s4, u4))
@@ -301,7 +302,13 @@ bool pd_t::zp_ok() {
     const bool weights_upconversion = wei_decomp_
             || ((swap_ab() ? b_int4 : a_int4) && dy_quant_enabled_);
 
-    if (attr_zps.has_host_scalars()) return false;
+    // Host scalar support
+    const bool a_zp_non_scalar = !a_zps.has_default_values() && ao_dims_ > 0;
+    const bool b_zp_non_scalar = !b_zps.has_default_values() && bo_dims_ > 0;
+    // Host scalar ZPs supported only for A & B. Both matrixes' ZPs should be scalar.
+    if (c_zps.is_host_scalar() || (a_zps.is_host_scalar() && b_zp_non_scalar)
+            || (b_zps.is_host_scalar() && a_zp_non_scalar))
+        return false;
 
     if (!a_zps.has_default_values()) {
         // Groups determine supported masks.
