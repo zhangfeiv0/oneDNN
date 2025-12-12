01ce2dc9735759eb9a94507ae41cf85923b8a637
zhangfei <zhangfei@iscas.ac.cn>
2025-12-12 13:45:10 +0800

    test

--- .github/automation/riscv/skipped-tests.sh
+++ .github/automation/riscv/skipped-tests.sh
@@ -22,38 +22,7 @@
 
 set -eo pipefail
 
-OS=${OS:-"Linux"}
-
-# described in issue: https://github.com/uxlfoundation/oneDNN/issues/2175
-SKIPPED_TESTS="test_benchdnn_modeC_matmul_multidims_cpu"
-
-#  We currently have some OS and config specific test failures.
-if [[ "$OS" == "Linux" ]]; then
-    SKIPPED_TESTS+="|test_benchdnn_modeC_graph_ci_cpu"
-fi
-
 # Skip these tests that will fail on CI for the RISC-V architecture.
-SKIPPED_TESTS+="|cpu-matmul-coo-cpp|cpu-matmul-csr-cpp|test_sum"
-
-# Skip time-consuming tests (QEMU is slow)
-SKIPPED_TESTS+="|cpu-cnn-training-f32-cpp"
-SKIPPED_TESTS+="|cpu-cnn-inference-f32-cpp"
-SKIPPED_TESTS+="|cpu-cnn-training-f32-c"
-SKIPPED_TESTS+="|cpu-graph-gated-mlp-int4-cpp"
-SKIPPED_TESTS+="|cpu-performance-profiling-cpp"
-SKIPPED_TESTS+="|cpu-rnn-training-f32-cpp"
-SKIPPED_TESTS+="|test_convolution_backward_data_f32"
-SKIPPED_TESTS+="|test_convolution_backward_weights_f32"
-SKIPPED_TESTS+="|test_convolution_eltwise_forward_f32"
-SKIPPED_TESTS+="|test_convolution_eltwise_forward_x8s8f32s32"
-SKIPPED_TESTS+="|test_convolution_forward_f32"
-SKIPPED_TESTS+="|test_pooling_backward"
-SKIPPED_TESTS+="|test_pooling_forward"
-SKIPPED_TESTS+="|test_gemm_f32"
-SKIPPED_TESTS+="|test_gemm_s8s8s32"
-SKIPPED_TESTS+="|test_gemm_u8s8s32"
-SKIPPED_TESTS+="|test_graph_unit_dnnl_mqa_decomp_cpu"
-SKIPPED_TESTS+="|test_graph_unit_dnnl_sdp_decomp_cpu"
-SKIPPED_TESTS+="|cpu-graph-sdpa-cpp"
+SKIPPED_TESTS="cpu-matmul-coo-cpp|cpu-matmul-csr-cpp|test_sum"
 
 echo "$SKIPPED_TESTS"
