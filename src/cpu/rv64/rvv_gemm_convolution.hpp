b049c525d4f39dd7a8fe874a9018a19c2b27942f
Xia Zhuozhao <i@xiazhuozhao.com>
2025-12-27 02:14:30 +0800

    cpu: riscv: add ISCAS copyright to new files
    Co-authored-by: Fei Zhang <zhangfei@iscas.ac.cn>

--- src/cpu/rv64/rvv_gemm_convolution.hpp
+++ src/cpu/rv64/rvv_gemm_convolution.hpp
@@ -1,5 +1,6 @@
 /*******************************************************************************
 * Copyright 2016 Intel Corporation
+* Copyright 2025 Institute of Software, Chinese Academy of Sciences
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
--- src/cpu/rv64/rvv_gemm_reorder.cpp
+++ src/cpu/rv64/rvv_gemm_reorder.cpp
@@ -1,6 +1,7 @@
 /*******************************************************************************
 * Copyright 2022 IBM Corporation
 * Copyright 2025 Intel Corporation
+* Copyright 2025 Institute of Software, Chinese Academy of Sciences
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
