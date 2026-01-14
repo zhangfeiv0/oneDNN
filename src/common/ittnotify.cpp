0d9547c292976ee9b1c2c808383bef57cdf2775f
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2026-01-13 16:37:19 -0800

    fixup: common: fixes for coverity major & moderate issues
    Restrict the range of allowed `kind_idx` values to task registration.

--- src/common/ittnotify.cpp
+++ src/common/ittnotify.cpp
@@ -102,9 +102,9 @@ void primitive_task_start(
 #undef CASE
     int kind_idx = (int)kind;
     assert(kind_idx >= 0);
-    if (kind_idx < primitive_kind::internal_only_start) {
-        assert((size_t)kind_idx < sizeof(prim_kind_itt_strings)
-                        / sizeof(prim_kind_itt_strings[0]));
+    if (kind_idx < primitive_kind::internal_only_start
+            && (size_t)kind_idx < sizeof(prim_kind_itt_strings)
+                            / sizeof(prim_kind_itt_strings[0])) {
         __itt_task_begin(pd_domain, __itt_null, __itt_null,
                 prim_kind_itt_strings[kind_idx]);
     }
